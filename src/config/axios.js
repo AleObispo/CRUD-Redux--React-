5cc70bb2e2181f40b410908d046fbc61ef2c51b1
AleObispo <aobispo@gmail.com>
2021-09-09 03:38:00 -0300

    cambios axios

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const clienteAxios = axios.create({
-    baseURL: 'http://localhost:4000/'
+    baseURL: 'https://app-crud-redux.herokuapp.com/'
 });
 
 export default clienteAxios;
+
+//'http://localhost:4000/'
